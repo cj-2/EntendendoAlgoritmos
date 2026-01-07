cf570d7113636efaab579aeb2787d3ade1bd8efc
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2026-01-07 10:29:06 -0300

    feat: adição do quicksort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using QuickSort;
+
+QuickSortInt.Print(QuickSortInt.Run([64, 34, 25, 12, 22, 11, 90, 5, 77, 30]));
+QuickSortInt.Print(QuickSortInt.Run([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+QuickSortInt.Print(QuickSortInt.Run([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+QuickSortInt.Print(QuickSortInt.Run([5, 2, 8, 2, 5, 1, 8, 8, 3, 1]));
+QuickSortInt.Print(QuickSortInt.Run([3, -1, 4, -2, 0, -5, 7, -3, 2]));
+
+// Casos Base:
+QuickSortInt.Print(QuickSortInt.Run([]));
+QuickSortInt.Print(QuickSortInt.Run([42]));
---
+++ QuickSort/QuickSortInt.cs
@@ -0,0 +1,46 @@
+﻿namespace QuickSort;
+
+public static class QuickSortInt
+{
+    public static List<int> Run(List<int> list)
+    {
+        // Casos de base, onde temos um item na lista ou dois.
+        if (list.Count <= 1) return list;
+        if (list.Count == 2)
+        {
+            var (a, b) = (list[0], list[1]);
+            return a > b ? [b, a] : [a, b];
+        }
+
+        var maiorQue = new List<int>();
+        var menorQue = new List<int>();
+
+        var pivoIdx = (list.Count - 1) / 2;
+        var pivo = list[pivoIdx];
+        list.RemoveAt(pivoIdx);
+
+        foreach (var i in list)
+        {
+            if (i > pivo)
+                maiorQue.Add(i);
+            else
+                menorQue.Add(i);
+        }
+
+        // Dividir para conquistar!
+        var result = new List<int>();
+        result.AddRange(Run(menorQue)); // Passamos "metade" para cá.
+        result.Add(pivo);
+        result.AddRange(Run(maiorQue)); // E "metate" pra cá.
+
+        return result;
+    }
+
+    public static void Print(List<int> list)
+    {
+        foreach (var i in list)
+            Console.Write(i + " ");
+
+        Console.WriteLine();
+    }
+}
