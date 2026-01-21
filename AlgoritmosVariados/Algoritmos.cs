081d4d028ebf55323ee3c1a7cb1079d170759d56
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2026-01-21 10:36:15 -0300

    feat: alguns algorítmos "genéricos" Não são os mais otimizados, mas são exemplos utilizando recursão e loops.

---
+++ AlgoritmosVariados/Algoritmos.cs
@@ -0,0 +1,63 @@
+﻿namespace AlgoritmosVariados;
+
+public static class Algoritmos
+{
+    private static int[] RemoverPrimeiroItem(int[] lista)
+    {
+        var novaLista = new int[lista.Length - 1];
+        Array.Copy(lista, 1, novaLista, 0, lista.Length - 1);
+        return novaLista;
+    }
+
+    public static int? BuscaMaior(int[] lista, int? maior = null)
+    {
+        if (lista.Length == 0) return maior;
+
+        var primeiro = lista[0];
+        var novaLista = RemoverPrimeiroItem(lista);
+
+        if (maior == null)
+            return BuscaMaior(novaLista, primeiro);
+
+        if (primeiro > maior)
+            return BuscaMaior(novaLista, primeiro);
+
+        return BuscaMaior(novaLista, maior);
+    }
+
+    public static int? BuscaMenor(int[] lista, int? menor = null)
+    {
+        if (lista.Length == 0)
+            return menor;
+
+        var idx = 1;
+        menor = lista[0];
+
+        while (idx < lista.Length)
+        {
+            if (lista[idx] < menor)
+                menor = lista[idx];
+
+            idx++;
+        }
+
+        return menor;
+    }
+
+    public static int Soma(int[] lista)
+    {
+        if (lista.Length == 0) return 0;
+
+        var primeiro = lista[0];
+        var novaLista = RemoverPrimeiroItem(lista);
+        return primeiro + Soma(novaLista);
+    }
+
+    public static int TamanhoLista(int[] lista)
+    {
+        if (lista.Length == 0) return 0;
+
+        var novaLista = RemoverPrimeiroItem(lista);
+        return 1 + TamanhoLista(novaLista);
+    }
+}
---
+++ AlgoritmosVariados/Program.cs
@@ -0,0 +1,18 @@
+﻿using AlgoritmosVariados;
+
+Console.WriteLine(Algoritmos.Soma([]));
+Console.WriteLine(Algoritmos.Soma([0]));
+Console.WriteLine(Algoritmos.Soma([5, 10, 15, -2, 2]));
+Console.WriteLine("---");
+Console.WriteLine(Algoritmos.TamanhoLista([]));
+Console.WriteLine(Algoritmos.TamanhoLista([5, 10, 15, -2, 2]));
+Console.WriteLine("---");
+Console.WriteLine(Algoritmos.BuscaMaior([]));
+Console.WriteLine(Algoritmos.BuscaMaior([42]));
+Console.WriteLine(Algoritmos.BuscaMaior([-2]));
+Console.WriteLine(Algoritmos.BuscaMaior([5, 10, 15, -2, 2]));
+Console.WriteLine("---");
+Console.WriteLine(Algoritmos.BuscaMenor([]));
+Console.WriteLine(Algoritmos.BuscaMenor([-42]));
+Console.WriteLine(Algoritmos.BuscaMenor([42]));
+Console.WriteLine(Algoritmos.BuscaMenor([5, 10, 15, -2, 2]));
