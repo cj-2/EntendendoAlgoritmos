e34d3fc736050d2767217d72d4e0fd479634ea17
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2026-01-28 11:13:59 -0300

    feat: algoritmo de busca em largura.

---
+++ PesquisaEmLargura/Program.cs
@@ -0,0 +1,17 @@
+﻿using PesquisaEmLargura;
+
+var friends = new Dictionary<string, string[]>();
+// ponto de partida, que no caso sou eu.
+friends["eu"] = ["jose", "vitor", "rodrigo"]; // meus vizinhos
+// primeiro grau
+friends["jose"] = ["germania"]; // vizinhos de jose
+friends["vitor"] = ["alice", "germania"];
+friends["rodrigo"] = ["eu"];
+// segundo grau
+friends["germania"] = [];
+friends["alice"] = ["tom"];
+// terceiro grau
+friends["tom"] = ["eu"];
+
+Console.WriteLine(Search.Handle(friends, "eu", "t")); // tom
+Console.WriteLine(Search.Handle(friends, "eu", "t")); // germania
---
+++ PesquisaEmLargura/Search.cs
@@ -0,0 +1,45 @@
+﻿namespace PesquisaEmLargura;
+
+public static class Search
+{
+    public static string? Handle(Dictionary<string, string[]> grafo, string start, string search)
+    {
+        var myQueue = new Queue<string>();
+        var alreadyVerified = new Dictionary<string, bool>();
+        alreadyVerified[start] = true;
+
+        foreach (var friend in grafo[start])
+        {
+            myQueue.Enqueue(friend);
+        }
+
+        while (true)
+        {
+            myQueue.TryDequeue(out var nextPerson);
+
+            if (nextPerson != null)
+            {
+                if (alreadyVerified.ContainsKey(nextPerson)) continue;
+                if (IsMangoSeller(nextPerson, search)) return nextPerson;
+
+                alreadyVerified[nextPerson] = true;
+
+                foreach (var nextFriends in grafo[nextPerson])
+                {
+                    myQueue.Enqueue(nextFriends);
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return "Not Found";
+    }
+
+    private static bool IsMangoSeller(string person, string search)
+    {
+        return person.StartsWith(search);
+    }
+}
