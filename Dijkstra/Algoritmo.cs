ed9583b13233d3b449c2e916b4afd139c2f34734
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2026-01-29 13:32:09 -0300

    feat: implementação do algoritmo dijkstra

---
+++ Dijkstra/Algoritmo.cs
@@ -0,0 +1,96 @@
+﻿namespace Dijkstra;
+
+public static class Algoritmo
+{
+    public static void Handle(Dictionary<string, Dictionary<string, int>> grafo, string partida, string chegada)
+    {
+        var processados = new List<string>();
+        var custos = new Dictionary<string, int?>();
+        var pais = new Dictionary<string, string>();
+
+        foreach (var v in grafo.Keys)
+            custos[v] = null;
+
+        custos[partida] = 0;
+
+        var vertice = AcharVerticeComMenorCustoNaoProcessado(custos, processados);
+        while (vertice != null)
+        {
+            if (vertice == chegada) break;
+
+            var custo = custos[vertice]; // Custo atual para chegar onde estamos.
+            var vizinhos = grafo[vertice];
+
+            foreach (var key in vizinhos.Keys)
+            {
+                // O novo custo dos vizinhos pode ser o custo de chegar onde estamos mais o custo para chegar nos vizinhos.
+                var novoCusto = custo + vizinhos[key];
+
+                // Se ficou mais "barato" chegar no vizinho, comparado com a tabela de custos, atualiza:
+                if (custos[key] > novoCusto || !custos[key].HasValue)
+                {
+                    custos[key] = novoCusto;
+                    pais[key] = vertice;
+                }
+            }
+
+            processados.Add(vertice);
+            vertice = AcharVerticeComMenorCustoNaoProcessado(custos, processados);
+        }
+
+        ImprimirResultado(custos, pais, chegada);
+    }
+
+    private static string? AcharVerticeComMenorCustoNaoProcessado(Dictionary<string, int?> custos,
+        List<string> processados)
+    {
+        string? veritce = null;
+        int? custoVertice = null;
+
+        foreach (var key in custos.Keys)
+        {
+            if (processados.Contains(key) || custos[key] == null)
+                continue;
+
+            if (custoVertice < custos[key])
+                continue;
+
+            veritce = key;
+            custoVertice = custos[key];
+        }
+
+        return veritce;
+    }
+
+    private static void ImprimirResultado(Dictionary<string, int?> custos, Dictionary<string, string> pais,
+        string chegada)
+    {
+        Console.WriteLine("Custos:");
+        foreach (var keyValue in custos)
+            Console.WriteLine(keyValue.Key + ": " + keyValue.Value);
+
+        Console.WriteLine();
+        Console.WriteLine("Pais:");
+        foreach (var keyValue in pais)
+            Console.WriteLine(keyValue.Key + ": " + keyValue.Value);
+
+        Console.WriteLine();
+        var melhorCaminho = "";
+        var proximo = chegada;
+
+        while (true)
+        {
+            melhorCaminho = proximo + melhorCaminho;
+            if (pais.TryGetValue(proximo, out var v))
+            {
+                proximo = v;
+                melhorCaminho = " => " + melhorCaminho;
+            }
+            else break;
+        }
+
+        Console.WriteLine("Melhor caminho até a chegada:");
+        Console.WriteLine(melhorCaminho);
+        Console.WriteLine();
+    }
+}
---
+++ Dijkstra/Program.cs
@@ -0,0 +1,55 @@
+﻿// Um grafo de grafos.
+
+using Dijkstra;
+
+var grafo = new Dictionary<string, Dictionary<string, int>>
+{
+    ["inicio"] = new()
+    {
+        ["a"] = 6,
+        ["b"] = 2
+    },
+    ["a"] = new()
+    {
+        ["fim"] = 1
+    },
+    ["b"] = new()
+    {
+        ["a"] = 3,
+        ["fim"] = 5,
+    },
+    ["fim"] = new()
+};
+
+Algoritmo.Handle(grafo, "inicio", "fim");
+
+var grafo2 = new Dictionary<string, Dictionary<string, int>>
+{
+    ["inicio"] = new()
+    {
+        ["a"] = 5,
+        ["b"] = 2
+    },
+    ["a"] = new()
+    {
+        ["c"] = 4,
+        ["d"] = 2
+    },
+    ["b"] = new()
+    {
+        ["a"] = 8,
+        ["d"] = 7,
+    },
+    ["c"] = new()
+    {
+        ["fim"] = 3,
+        ["d"] = 6,
+    },
+    ["d"] = new()
+    {
+        ["fim"] = 1,
+    },
+    ["fim"] = new()
+};
+
+Algoritmo.Handle(grafo2, "inicio", "fim");
